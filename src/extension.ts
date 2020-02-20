ee76fbb2716821c0955024d5a3e92d00f89f322d
Gabriel Alencar <pixcel97@gmail.com>
2020-02-20 12:41:07 -0300

    Problema duplicidade de documentação
    - Corrigido problema que ocorria ao abrir os fontes após a atualização da tabela, estava duplicando as documentações.

--- src/extension.ts
+++ src/extension.ts
@@ -102,9 +102,8 @@ export function searchProtheusDocInFile(text: string, uri: vscode.Uri) {
 	let expressionProtheusDoc = /(\{Protheus\.doc\}\s*)([^*]*)(\n[^:\n]*)/mig;
 	let match = text.match(expressionProtheusDoc);
 
-	// FIXME: Identificado que ao abrir os fontes após a atualização da tabela, está duplicando as documentações.
 	// Remove todas as referências de documentação do arquivo aberto
-	documentations = documentations.filter(doc => doc.file !== uri);
+	documentations = documentations.filter(doc => doc.file.fsPath !== uri.fsPath);
 
 	if (match) {
 		// Percorre via expressão regular todas as ocorrencias de ProtheusDoc no arquivo.
